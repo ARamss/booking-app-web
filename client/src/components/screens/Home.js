6186775ec3ebf902f3fcb53ce62974e39146c4e6
Alberto Ramos <albertoramos@Albertos-Mac-mini.local>
2021-11-14 18:19:57 -0600

    data fetch home

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,50 +1,23 @@
-import React from 'react';
+import React,{useState,useEffect,useContext} from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import { Card, Text, Button, Grid } from '@nextui-org/react';
+import {UserContext} from '../../App'
+import {Link} from 'react-router-dom'
 
  const Home = ()=>{
-       const list = [
-         {
-           title: 'Orange',
-           img: '/images/fruit-1.jpeg',
-           price: '$5.50'
-         },
-         {
-           title: 'Tangerine',
-           img: '/images/fruit-2.jpeg',
-           price: '$3.00'
-         },
-         {
-           title: 'Raspberry',
-           img: '/images/fruit-3.jpeg',
-           price: '$10.00'
-         },
-         {
-           title: 'Lemon',
-           img: '/images/fruit-4.jpeg',
-           price: '$5.30'
-         },
-         {
-           title: 'Advocato',
-           img: '/images/fruit-5.jpeg',
-           price: '$15.70'
-         },
-         {
-           title: 'Lemon 2',
-           img: '/images/fruit-6.jpeg',
-           price: '$8.00'
-         },
-         {
-           title: 'Banana',
-           img: '/images/fruit-7.jpeg',
-           price: '$7.50'
-         },
-         {
-           title: 'Watermelon',
-           img: '/images/fruit-8.jpeg',
-           price: '$12.20'
-         }
-     ];
+    const [data,setData] = useState([])
+    const {state,dispatch} = useContext(UserContext)
+    useEffect(()=>{
+      fetch('/todas-las-instalaciones',{
+          headers:{
+              "Authorization":"Bearer "+localStorage.getItem("jwt")
+          }
+      }).then(res=>res.json())
+      .then(result=>{
+          console.log(result)
+          setData(result.instalaciones)
+      })
+    },[])
 
     return(
         <div>
@@ -52,32 +25,31 @@ import { Card, Text, Button, Grid } from '@nextui-org/react';
         <Col sm>
             <div style={{backgroundColor:'#FFF'}}>
                   <Grid.Container gap={1} justify="flex-start">
-                      {list.map((item, index) => (
-                          <Grid xs={6} sm={3} key={index}>
-                              <Card hoverable clickable width="100%">
+                      {
+                        data.map(item => (
+                          <Grid xs={6} sm={3}>
+                              <Card hoverable clickable width="100%" key={item._id}>
                                   <Card.Body noPadding>
                                       <Card.Image
                                           objectFit="cover"
                                           autoResize={false}
-                                          src={item.img}
+                                          src={item.photo}
                                           width='100%'
                                           height={140}
-                                          alt={item.title}
+                                          alt={item.name}
                                       />
                                   </Card.Body>
                                   <Card.Footer justify="flex-start">
                                       <Row justify="space-between">
                                           <Text b>
-                                              {item.title}
-                                          </Text>
-                                          <Text weight={500} style={{ opacity: 0.6 }}>
-                                              {item.price}
+                                              {item.name}
                                           </Text>
                                       </Row>
                                   </Card.Footer>
                               </Card>
                           </Grid>
-                      ))}
+                      ))
+                    }
                   </Grid.Container>
             </div>
         </Col>
