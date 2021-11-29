cc005dbe523a3a967fce6a806c2465e2c6f9e32b
Alberto Ramos <albertoramos@Albertos-Mac-mini.local>
2021-11-29 16:03:45 -0600

    deploy

--- app.js
+++ app.js
@@ -32,3 +32,17 @@ app.use(require('./routes/instalacion'))
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
+
+//Production
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+          res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
